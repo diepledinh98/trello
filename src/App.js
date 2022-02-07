5a07259f24602f4a51ee5f2aba314210c3b6a7b8
diepledinh98 <ledinhdiep123456789@gmail.com>
2022-02-07 20:51:26 +0700

    Clean_Architecture

--- src/App.js
+++ src/App.js
@@ -1,123 +1,14 @@
 import './App.scss';
+import AppBar from './components/AppBar/AppBar';
+import BoardBar from './components/BoardBar/BoardBar';
+import BoardContent from './components/BoardContent/BoardContent';
 
 function App() {
     return (
         <div className="trello">
-            <nav className='navbar app'>App Bar</nav>
-            <nav className='navbar board'>Board Bar</nav>
-            <div className='board-column'>
-                <div className='column'>
-                    <header>
-                        Brain Storm
-                    </header>
-                    <ul>
-                        <li>
-                            <img src='https://media.hoclaptrinh.vn/images/trello-la-gi-5c36cae13c513.jpg' alt='dieple' />
-                        </li>
-                        <li>Ăn tết xong kiếm tiền</li>
-                        <li>Ăn tết xong kiếm tiền</li>
-                        <li>Ăn tết xong kiếm tiền</li>
-                        <li>Ăn tết xong kiếm tiền</li>
-
-                        <li>Ăn tết xong kiếm tiền</li>
-                    </ul>
-
-                    <footer>Add another card</footer>
-                </div>
-                <div className='column'>
-                    <header>
-                        Brain Storm
-                    </header>
-                    <ul>
-                        <li>
-
-                            <img src='https://media.hoclaptrinh.vn/images/trello-la-gi-5c36cae13c513.jpg' alt='dieple' />
-                        </li>
-                        <li>Ăn tết xong kiếm tiền</li>
-                        <li>Ăn tết xong kiếm tiền</li>
-                        <li>Ăn tết xong kiếm tiền</li>
-                        <li>Ăn tết xong kiếm tiền</li>
-                        <li>Ăn tết xong kiếm tiền</li>
-                    </ul>
-
-                    <footer>Add another card</footer>
-                </div>
-
-                <div className='column'>
-                    <header>
-                        Brain Storm
-                    </header>
-                    <ul>
-                        <li>
-                            <img src='https://media.hoclaptrinh.vn/images/trello-la-gi-5c36cae13c513.jpg' alt='dieple' />
-                        </li>
-                        <li>Ăn tết xong kiếm tiền</li>
-                        <li>Ăn tết xong kiếm tiền</li>
-                        <li>Ăn tết xong kiếm tiền</li>
-                        <li>Ăn tết xong kiếm tiền</li>
-
-                        <li>Ăn tết xong kiếm tiền</li>
-                    </ul>
-
-                    <footer>Add another card</footer>
-                </div>
-                <div className='column'>
-                    <header>
-                        Brain Storm
-                    </header>
-                    <ul>
-                        <li>
-                            <img src='https://media.hoclaptrinh.vn/images/trello-la-gi-5c36cae13c513.jpg' alt='dieple' />
-                        </li>
-                        <li>Ăn tết xong kiếm tiền</li>
-                        <li>Ăn tết xong kiếm tiền</li>
-                        <li>Ăn tết xong kiếm tiền</li>
-                        <li>Ăn tết xong kiếm tiền</li>
-
-                        <li>Ăn tết xong kiếm tiền</li>
-                        <li>Ăn tết xong kiếm tiền</li>
-                    </ul>
-
-                    <footer>Add another card</footer>
-                </div>
-                <div className='column'>
-                    <header>
-                        Brain Storm
-                    </header>
-                    <ul>
-                        <li>
-                            <img src='https://media.hoclaptrinh.vn/images/trello-la-gi-5c36cae13c513.jpg' alt='dieple' />
-                        </li>
-                        <li>Ăn tết xong kiếm tiền</li>
-                        <li>Ăn tết xong kiếm tiền</li>
-                        <li>Ăn tết xong kiếm tiền</li>
-                        <li>Ăn tết xong kiếm tiền</li>
-                        <li>Ăn tết xong kiếm tiền</li>
-                        <li>Ăn tết xong kiếm tiền</li>
-
-                    </ul>
-
-                    <footer>Add another card</footer>
-                </div>
-                <div className='column'>
-                    <header>
-                        Brain Storm
-                    </header>
-                    <ul>
-                        <li>
-                            <img src='https://media.hoclaptrinh.vn/images/trello-la-gi-5c36cae13c513.jpg' alt='dieple' />
-                        </li>
-                        <li>Ăn tết xong kiếm tiền</li>
-                        <li>Ăn tết xong kiếm tiền</li>
-
-                        <li>Ăn tết xong kiếm tiền</li>
-                        <li>Ăn tết xong kiếm tiền</li>
-                        <li>Ăn tết xong kiếm tiền</li>
-                    </ul>
-
-                    <footer>Add another card</footer>
-                </div>
-            </div>
+            <AppBar />
+            <BoardBar />
+            <BoardContent />
         </div>
     );
 }
---
+++ src/components/Task/Task.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+import './Task.scss';
+
+function Task() {
+    return (
+        <li>
+            <img src='https://media.hoclaptrinh.vn/images/trello-la-gi-5c36cae13c513.jpg' alt='dieple' />
+        </li>
+    )
+}
+
+export default Task;
